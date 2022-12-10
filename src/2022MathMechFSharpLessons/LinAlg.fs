71396986aa05426c1c42b6410ca452607aa6e183
Alin42 <118alin@rambler.ru>
2022-12-10 23:44:25 +0300

    Tests pass.. But repository is about to burn

--- src/2022MathMechFSharpLessons/LinAlg.fs
+++ src/2022MathMechFSharpLessons/LinAlg.fs
@@ -6,13 +6,14 @@ module LinAlg =
     type ITensorTree<'elementType when 'elementType: equality> =
         abstract member size: int
         abstract member _actualSize: int
-        abstract member value: 'elementType option
+        // abstract member data: 'elementType option or ITensorTree<'elementType> option
         abstract member depth: int
 
     type ITensor<'elementType when 'elementType: equality> =
-        // abstract member list: list
+        // abstract member list: list of same demention as tensor
         abstract member transpose: ITensor<'elementType>
 
+    // Function to calculate 2**X, which is infinum of all 2**x >= n
     let _getMin2Pow (n: int) =
         let getRidOfZero (twoPow: int) (n: int) = n ||| (n >>> twoPow)
 
@@ -27,7 +28,7 @@ module LinAlg =
     // TODO:
     //  check preconditions: |arr| > 0, |arr.[i]| = |arr.[j]| > 0 for each i,j ONLY when flag debug is active
     //  add postcondion-checks in tests
-    //  make sure equality is needed everywhere it is used
+    //  make access to trees private
 
     //  This is not sparse at all, I still need to add relaxation XD
 
@@ -37,7 +38,7 @@ module LinAlg =
             _actualSize_: int,
             a0: VectorBinTree<'elementType> option,
             a1: VectorBinTree<'elementType> option,
-            value_: 'elementType option,
+            data_: 'elementType option,
             depth_: int
         ) =
         struct
@@ -45,73 +46,64 @@ module LinAlg =
             member this._actualSize: int = _actualSize_
             member this.left: VectorBinTree<'elementType> option = a0
             member this.right: VectorBinTree<'elementType> option = a1
-            member this.value: 'elementType option = value_
+            member this.data: 'elementType option = data_
             member this.depth: int = depth_
 
             member this.listWithTail(tail: 'elementType list) =
-                if this.value.IsSome then
-                    [ this.value.Value ] @ tail
+                if this.data.IsSome then
+                    let data = this.data.Value
+                    let amount = 1 <<< this.depth
+                    (List.init amount (fun _ -> data)) @ tail
                 elif this.left = None then
                     tail
                 elif this.right = None then
                     this.left.Value.listWithTail tail
                 else
-                    let newTail = this.right.Value.listWithTail tail // rvalue needed -_-
+                    let newTail = this.right.Value.listWithTail tail // 'this.' needed -_-
                     this.left.Value.listWithTail newTail
 
             member this.list: 'elementType list = this.listWithTail []
 
-            new(arr: 'elementType list, _actualSize__: int option) =
+            new(arr: 'elementType list, _actualSize__: int) =
                 let size__ = arr.Length
-
                 if size__ = 0 then
                     raise
                     <| new Exception("Can't create empty vector")
 
-                let _actualSize__ =
-                    if _actualSize__.IsSome then
-                        _actualSize__.Value
-                    else
-                        _getMin2Pow (size__)
-
                 let a0 =
                     if _actualSize__ > 1 then
-                        Some(new VectorBinTree<'elementType>(arr.[.. (_actualSize__ / 2 - 1)], Some(_actualSize__ / 2)))
+                        Some(new VectorBinTree<'elementType>(arr.[.. (_actualSize__ / 2 - 1)], _actualSize__ / 2))
                     else
                         None
-
                 let a1 =
                     if _actualSize__ > 1 && size__ > _actualSize__ / 2 then
-                        Some(new VectorBinTree<'elementType>(arr.[(_actualSize__ / 2) ..], Some(_actualSize__ / 2)))
+                        Some(new VectorBinTree<'elementType>(arr.[(_actualSize__ / 2) ..], _actualSize__ / 2))
                     else
                         None
-
-                if size__ = 1 then
-                    new VectorBinTree<'elementType>(size__, _actualSize__, None, None, Some(arr.[0]), 1)
-                elif a0.Value.value.IsSome
-                     && a1.Value.value.IsSome
-                     && a0.Value.value.Value = a1.Value.value.Value then // T_T
+                if _actualSize__ = 1 then
+                    new VectorBinTree<'elementType>(size__, _actualSize__, None, None, Some(arr.[0]), 0)
+                elif a0 = None && a1 = None then
+                    new VectorBinTree<'elementType>(size__, _actualSize__, None, None, None, 0)
+                elif a0.IsSome
+                     && a0.Value.data.IsSome
+                     && a1.IsSome
+                     && a1.Value.data.IsSome
+                     && (a0.Value.data.Value = a1.Value.data.Value) then // T_T
                     new VectorBinTree<'elementType>(
                         size__,
                         _actualSize__,
                         None,
                         None,
-                        a0.Value.value,
+                        a0.Value.data,
                         a0.Value.depth + 1
                     )
                 else
                     new VectorBinTree<'elementType>(size__, _actualSize__, a0, a1, None, a0.Value.depth + 1)
+
+            new(arr: 'elementType list) = new VectorBinTree<'elementType>(arr, _getMin2Pow <| arr.Length)
         end
 
-    type Vector<'elementType when 'elementType: equality>(tree_: VectorBinTree<'elementType>) =
-        struct
-            member this.tree: VectorBinTree<'elementType> = tree_
-            member this.list = this.tree.list
-
-            new(arr: 'elementType list) = new Vector<'elementType>(new VectorBinTree<'elementType>(arr, None))
-        end
-
-    type MatrixQuadTree<'elementType when 'elementType: equality>
+    and MatrixQuadTree<'elementType when 'elementType: equality>
         (
             sizeH_: int,
             sizeW_: int,
@@ -121,7 +113,7 @@ module LinAlg =
             a01: MatrixQuadTree<'elementType> option,
             a10: MatrixQuadTree<'elementType> option,
             a11: MatrixQuadTree<'elementType> option,
-            data_: VectorBinTree<'elementType list> option,
+            data_: VectorBinTree<'elementType> option,
             depth_: int
         ) =
         struct
@@ -133,12 +125,15 @@ module LinAlg =
             member this.rightUp: MatrixQuadTree<'elementType> option = a01
             member this.leftDown: MatrixQuadTree<'elementType> option = a10
             member this.rightDown: MatrixQuadTree<'elementType> option = a11
-            member this.data: VectorBinTree<'elementType list> option = data_
+            member this.data: VectorBinTree<'elementType> option = data_
             member this.depth: int = depth_
 
             member this.list: 'elementType list list =
                 if this.data.IsSome then
-                    this.data.Value.list
+                    if this.sizeH = 1 then
+                        [ this.data.Value.list ]
+                    else // sizeW = 1 -> transpose vector
+                        List.transpose [ this.data.Value.list ]
                 else
                     let leftPart =
                         if this.leftUp <> None && this.leftDown <> None then
@@ -158,8 +153,7 @@ module LinAlg =
                         else
                             []
 
-                    List.map (fun (r, l) -> r @ l)
-                    <| List.zip leftPart rightPart
+                    List.map2 (fun l r -> l @ r) leftPart rightPart
 
             new(arr: 'elementType list list, _actualSizeH__: int option, _actualSizeW__: int option) =
                 let sizeH = List.length arr
@@ -194,8 +188,8 @@ module LinAlg =
                                 List.map
                                     (fun (line: 'elementType list) -> line.[.. (_actualSizeW / 2 - 1)])
                                     (arr.[.. (_actualSizeH / 2 - 1)]),
-                                None,
-                                None
+                                Some(_actualSizeH / 2),
+                                Some(_actualSizeW / 2)
                             )
                         )
                     else
@@ -208,8 +202,8 @@ module LinAlg =
                                 List.map
                                     (fun (line: 'elementType list) -> line.[(_actualSizeW / 2) ..])
                                     (arr.[.. (_actualSizeH / 2 - 1)]),
-                                None,
-                                None
+                                Some(_actualSizeH / 2),
+                                Some(_actualSizeW / 2)
                             )
                         )
                     else
@@ -222,8 +216,8 @@ module LinAlg =
                                 List.map
                                     (fun (line: 'elementType list) -> line.[.. (_actualSizeW / 2 - 1)])
                                     (arr.[(_actualSizeH / 2) ..]),
-                                None,
-                                None
+                                Some(_actualSizeH / 2),
+                                Some(_actualSizeW / 2)
                             )
                         )
                     else
@@ -236,16 +230,23 @@ module LinAlg =
                                 List.map
                                     (fun (line: 'elementType list) -> line.[(_actualSizeW / 2) ..])
                                     (arr.[(_actualSizeH / 2) ..]),
-                                None,
-                                None
+                                Some(_actualSizeH / 2),
+                                Some(_actualSizeW / 2)
                             )
                         )
                     else
                         None
 
-                let data: VectorBinTree<'elementType list> option =
-                    if sizeH = 1 || sizeW = 1 then
-                        Some(new VectorBinTree<'elementType list>(arr, Some(_actualSizeW * _actualSizeH)))
+                let data: VectorBinTree<'elementType> option =
+                    if sizeH = 1 then
+                        Some(new VectorBinTree<'elementType>(arr.[0], _actualSizeW))
+                    elif sizeW = 1 then
+                        Some(
+                            new VectorBinTree<'elementType>(
+                                List.foldBack (fun (el: 'elementType list) acc -> el.[0] :: acc) arr [],
+                                _actualSizeH
+                            )
+                        )
                     else
                         None
 
@@ -253,7 +254,7 @@ module LinAlg =
                     if leftUp.IsSome then
                         leftUp.Value.depth + 1
                     elif data.IsSome then
-                        data.Value.depth + 1
+                        data.Value.depth
                     else
                         1
 
@@ -271,15 +272,29 @@ module LinAlg =
                 )
         end
 
+    type Vector<'elementType when 'elementType: equality>(tree_: VectorBinTree<'elementType>) =
+        struct
+            member this.tree: VectorBinTree<'elementType> = tree_
+            member this.list = this.tree.list
+
+            new(arr: 'elementType list) = new Vector<'elementType>(new VectorBinTree<'elementType>(arr))
+        end
+
     type Matrix<'elementType when 'elementType: equality>(tree_: MatrixQuadTree<'elementType>) =
         struct
-            member this.tree = tree_
-
+            member this.tree: MatrixQuadTree<'elementType> = tree_
             member this.list = this.tree.list
 
             new(arr: 'elementType list list) =
                 new Matrix<'elementType>(new MatrixQuadTree<'elementType>(arr, None, None))
         end
+
+    let rec mult (A: Matrix<'elementType>) (v: Vector<'elementType>) = 
+        if A.tree.sizeW <> v.tree.size then
+            raise <| new Exception("Multiplication of matrix %d{A.tree.sizeH} x %d{A.tree.sizeW} and vector %d{v.tree.size}")
+        
+    //let mult (v: Vector) (B: Matrix<>) = 
+    //let mult (A: Matrix) (B: Matrix<>) = 
 
     let rec transposeMatrixTree<'elementType when 'elementType: equality> (tree: MatrixQuadTree<'elementType>) = //(s: int) (t: int) =
         new MatrixQuadTree<'elementType>(
--- tests/2022MathMechFSharpLessons.Tests/LinAlgTests.fs
+++ tests/2022MathMechFSharpLessons.Tests/LinAlgTests.fs
@@ -28,6 +28,7 @@ module LinAlgTests =
               <| fun _ ->
                   let a: Vector<int> = Vector([ 1; 2; 3; 4; 5; 6 ])
 
+                  Expect.equal a.tree.depth 3 "Vector [1-6] depth is not 3"
                   Expect.equal a a "Check vector" // TODO: actual tests
               testCase "Small square matrix transpose test"
               <| fun _ ->
@@ -35,14 +36,15 @@ module LinAlgTests =
                   let b: Matrix<int> = Matrix([ [ 1; 3 ]; [ 2; 4 ] ])
                   let subject = transpose a
 
-                  Expect.equal subject.list b.list "Operation transpose does not work on a small matrix"
+                  Expect.equal subject.list b.list "Operation transpose does not work on a small square matrix"
               testCase "Small matrix transpose test"
               <| fun _ ->
                   let a: Matrix<int> = Matrix([ [ 1; 2; 3 ]; [ 4; 5; 6 ] ])
                   let b: Matrix<int> = Matrix([ [ 1; 4 ]; [ 2; 5 ]; [ 3; 6 ] ])
                   let subject = transpose a
-
-                  Expect.equal subject.list b.list "Operation transpose does not work on a small matrix"
+                  Expect.equal subject.list b.list "Operation transpose does not work on a small matrix (2*3 -> 3*2)"
+                  let subject = transpose b
+                  Expect.equal subject.list a.list "Operation transpose does not work on a small matrix (3*2 -> 2*3)"
               testCase "Depth of a small matrix test"
               <| fun _ ->
                   let subject: Matrix<int> =
@@ -52,10 +54,10 @@ module LinAlgTests =
                             [ 7; 8; 9 ] ]
                       )
 
-                  Expect.equal subject.tree.depth 3 "Depth of a matrix 3*3 should be 3"
+                  Expect.equal subject.tree.depth 2 "Depth of a matrix 3*3 should be 3"
                   let subject: Matrix<int> = Matrix([ [ 1; 2; 3 ]; [ 4; 5; 6 ] ])
 
-                  Expect.equal subject.tree.depth 3 "Depth of a matrix 2*3 should be 3"
+                  Expect.equal subject.tree.depth 2 "Depth of a matrix 2*3 should be 3"
 
                   let subject: Matrix<int> =
                       Matrix(
@@ -65,7 +67,7 @@ module LinAlgTests =
                             [ 10; 11; 12 ] ]
                       )
 
-                  Expect.equal subject.tree.depth 3 "Depth of a matrix 3*4 should be 3"
+                  Expect.equal subject.tree.depth 2 "Depth of a matrix 3*4 should be 3"
               testCase "Depth of a small sparse matrix test"
               <| fun _ ->
                   let subject: Matrix<int> =
@@ -75,9 +77,9 @@ module LinAlgTests =
                             [ 1; 1; 1 ] ]
                       )
 
-                  Expect.equal subject.tree.depth 3 "Depth of a sparse matrix 3*3 should be 3"
+                  Expect.equal subject.tree.depth 2 "Depth of a sparse matrix 3*3 should be 3"
                   let subject: Matrix<int> = Matrix([ [ 1; 1; 1 ]; [ 1; 1; 1 ] ])
-                  Expect.equal subject.tree.depth 3 "Depth of a sparse matrix 2*3 should be 3"
+                  Expect.equal subject.tree.depth 2 "Depth of a sparse matrix 2*3 should be 3"
 
                   let subject: Matrix<int> =
                       Matrix(
@@ -87,4 +89,4 @@ module LinAlgTests =
                             [ 1; 1; 1 ] ]
                       )
 
-                  Expect.equal subject.tree.depth 3 "Depth of a sparse matrix 3*4 should be 3" ]
+                  Expect.equal subject.tree.depth 2 "Depth of a sparse matrix 3*4 should be 3" ]
