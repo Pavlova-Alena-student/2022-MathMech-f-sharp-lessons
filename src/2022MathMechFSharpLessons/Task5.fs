fee06414dfeb2f750a50703d63a9c1f0292e9edd
alin42 <118alin@rambler.ru>
2023-04-13 13:47:51 +0300

    One test passes :)

--- src/2022MathMechFSharpLessons/Task5.fs
+++ src/2022MathMechFSharpLessons/Task5.fs
@@ -6,31 +6,48 @@ module Task5 =
     type Graph = { adjacency: Matrix<int> }
 
     type GraphVertexes =
-        { properties: Vector<int> }
+        | IntProperties of Vector<int>
+        | PositiveProperties of Vector<uint>
+        | BinProperties of Vector<bool>
         member this.tickFront(g: Graph) : GraphVertexes =
-            { properties = this.properties.mult (+) (*) g.adjacency }
+            match this with
+            | IntProperties (props) -> IntProperties(props.mult (+) (*) g.adjacency)
+            | PositiveProperties (props) -> PositiveProperties(props.mult (+) (fun a b -> a * uint (b)) g.adjacency)
+            | BinProperties (props) -> BinProperties(props.mult (||) (fun p value -> p && (value <> 0)) g.adjacency)
+
+        member this.length =
+            match this with
+            | IntProperties (props) -> props.length
+            | PositiveProperties (props) -> props.length
+            | BinProperties (props) -> props.length
 
         member this.notIn(other: GraphVertexes) : bool =
-            let masked =
-                Vector.map2 (fun a b -> if b = 0 then a else 0) this.properties other.properties
+            match this, other with
+            | BinProperties (thisProps), BinProperties (otherProps) ->
+                let masked =
+                    Vector.map2 (fun a b -> if b = false then a else false) thisProps otherProps
 
-            let rec allZero (tree: VectorBinTree<int>) : bool =
-                match tree with
-                | VLeaf (None, _) -> true
-                | VLeaf (Some (0), _) -> true
-                | VNode (l, r) -> (allZero l) && (allZero r)
-                | _ -> false
+                let rec allZero (tree: VectorBinTree<bool>) : bool = // TODO: NO VECTORBINTREEEEE!!!
+                    match tree with
+                    | VLeaf (None, _) -> true
+                    | VLeaf (Some (false), _) -> true
+                    | VNode (l, r) -> (allZero l) && (allZero r)
+                    | _ -> false
 
-            not (allZero (masked.tree))
+                not (allZero (masked.tree))
+            | _ -> false // TODO: What does "in" mean in other cases?
 
         static member op_Addition(a: GraphVertexes, b: GraphVertexes) =
-            { properties = Vector.map2 (+) a.properties b.properties }
+            match a, b with
+            | IntProperties (a), IntProperties (b) -> IntProperties(Vector.map2 (+) a b)
+            | PositiveProperties (a), PositiveProperties (b) -> PositiveProperties(Vector.map2 (+) a b)
+            | BinProperties (a), BinProperties (b) -> BinProperties(Vector.map2 (||) a b)
 
     let BFS (front: GraphVertexes) (graph: Graph) : GraphVertexes =
-        let rec BFS (front: GraphVertexes) (graph: Graph) (visited: GraphVertexes) : GraphVertexes =
-            if front.notIn visited then
-                BFS(front.tickFront graph) graph (visited + front)
+        let rec BFS (front: GraphVertexes) (graph: Graph) (acc: GraphVertexes) : GraphVertexes =
+            if front.notIn acc then
+                BFS(front.tickFront graph) graph (acc + front)
             else
-                visited
+                acc
 
-        BFS front graph { properties = Vector(List.replicate front.properties.length 0) }
+        BFS front graph (BinProperties(Vector(List.replicate front.length false)))
--- tests/2022MathMechFSharpLessons.Tests/Task5Tests.fs
+++ tests/2022MathMechFSharpLessons.Tests/Task5Tests.fs
@@ -7,6 +7,33 @@ module Task5Tests =
     open Task5
     open LinAlg
 
+    // 1-2; 1-4; 4-5
+    let smallGraph: Graph =
+        { adjacency =
+            Matrix(
+                [ [ 0; 1; 0; 1; 0 ]
+                  [ 1; 0; 0; 0; 0 ]
+                  [ 0; 0; 0; 0; 0 ]
+                  [ 1; 0; 0; 0; 1 ]
+                  [ 0; 0; 0; 1; 0 ] ]
+            ) }
+
+    let midGraph: Graph =
+        { adjacency =
+            Matrix(
+                [ [ 0; 1; 1; 0; 0; 0; 0; 0; 0; 0; 0 ]
+                  [ 1; 0; 0; 1; 0; 0; 0; 0; 0; 3; 0 ]
+                  [ 1; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0 ]
+                  [ 0; 1; 0; 0; 1; 0; 0; 0; 0; 0; 1 ]
+                  [ 0; 0; 0; 1; 0; 1; 0; 0; 0; 0; 0 ]
+                  [ 0; 0; 0; 0; 1; 0; 0; 0; 0; 0; 1 ]
+                  [ 0; 0; 0; 0; 0; 0; 0; 1; 1; 0; 0 ]
+                  [ 0; 0; 0; 0; 0; 0; 1; 0; 1; 0; 0 ]
+                  [ 0; 0; 0; 0; 0; 0; 1; 1; 0; 1; 0 ]
+                  [ 0; 3; 0; 0; 0; 0; 0; 0; 1; 0; 0 ]
+                  [ 0; 0; 0; 1; 0; 1; 0; 0; 0; 0; 0 ] ]
+            ) }
+
     [<Tests>]
     let tests =
         testList
@@ -14,22 +41,28 @@ module Task5Tests =
             [ testList "Tests for BFS-related functions" []
               testList
                   "BFS for connected components problem"
-                  [ testCase "Example of solving connected components problem"
+                  [ testCase "Example of solving connected to one vertex component problem"
                     <| fun _ ->
-                        // 1-2; 1-4; 4-5
-                        let g: Graph =
-                            { adjacency =
-                                Matrix(
-                                    [ [ 0; 1; 0; 1; 0 ]
-                                      [ 1; 0; 0; 0; 0 ]
-                                      [ 0; 0; 0; 0; 0 ]
-                                      [ 1; 0; 0; 0; 1 ]
-                                      [ 0; 0; 0; 1; 0 ] ]
-                                ) }
+                        let front: GraphVertexes =
+                            BinProperties(Vector([ true; false; false; false; false ]))
 
-                        let front: GraphVertexes = { properties = Vector([ 1; 0; 0; 0; 0 ]) }
-                        let subject = BFS front g
-                        let result = [ 1; 1; 0; 1; 1 ]
-                        Expect.equal (subject.properties.list ()) result "Failed to find connected component" ]
+                        let subject =
+                            match BFS front smallGraph with
+                            | BinProperties (prop) -> prop.list ()
+                            | _ -> []
+
+                        let result = [ true; true; false; true; true ]
+                        Expect.equal subject result "Failed to find a connected component"
+                    testCase "Example of solving connected components problem"
+                    <| fun _ ->
+                        let front: GraphVertexes = IntProperties(Vector([ 1; 2; 3; 4; 5 ]))
+
+                        let subject =
+                            match BFS front smallGraph with
+                            | IntProperties (prop) -> prop.list ()
+                            | _ -> []
+
+                        let result = [ 1; 1; 3; 1; 1 ]
+                        Expect.equal subject result "Failed to find connected components" ]
               testList "BFS for shortest path problem" []
               testList "BFS for shortest circle problem" [] ]
